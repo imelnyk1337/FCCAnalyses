c0a1b35027fdf70491e1af983c0ba85ce614386b
Ihor Melnyk <ihor.melnyk1337@gmail.com>
2022-08-05 14:32:29 +0200

    Changed perf.cpp

--- tests/perf/perf.cpp
+++ tests/perf/perf.cpp
@@ -5,17 +5,60 @@
 #include "vector"
 #include "string"
 #include "TCanvas.h"
+#include "TChain.h"
 #include "TInterpreter.h"
 #include "TH1D.h"
 #include "FCCAnalyses/ReconstructedParticle.h"
+#include <filesystem>
 
 
 //#define nCPUS 4
 #define outputDir "/home/imelnyk/FCCAnalyses/build/tests/perf/"
 #define outputFile "outputTest.root"
-#define inputDir "/home/imelnyk/FCCAnalyses/test_rootdir/"
+#define inputDir "/home/imelnyk/FCCAnalyses/p8_ee_ZH_ecm240/"
+#define sampleSize 40
 
 using namespace ROOT;
+
+TChain* retrieveData(int breakPoint) {
+
+    std::vector<std::string> files;
+    TChain* rootChain = new TChain("events");
+
+    std::cout << "#############################################################################################################" << std::endl;
+    std::cout << "#############################################################################################################" << std::endl;
+    std::cout << "                       Files retrieving starts: " << breakPoint << " files are going to be processed         " << std::endl;
+    std::cout << "#############################################################################################################" << std::endl;
+    std::cout << "#############################################################################################################" << std::endl;
+    std::cout << std::endl;
+
+
+    int currentPoint = 1;
+    for (const auto & entry : std::filesystem::directory_iterator(inputDir)) {
+        if (currentPoint == breakPoint) break;
+        std::string file = entry.path().string();
+//        char* var = file.data();
+//        std::vector<char> writable(file.begin(), file.end());
+//        writable.push_back('\0');
+//        files.push_back(entry.path().string());
+//        rootChain->Add(&writable[0]);
+        rootChain->Add(file.c_str());
+        std::cout << "  File " << file.c_str() << " was added to the ROOT::TChain object " << std::endl;
+//        std::cout << "  File " << &writable[0] << " was added to the ROOT::TChain object " << std::endl;
+        currentPoint++;
+    }
+
+    std::cout << std::endl;
+    std::cout << "#############################################################################################################" << std::endl;
+    std::cout << "#############################################################################################################" << std::endl;
+    std::cout << "                       Files retrieving was finished: " << breakPoint << " files had been processed          " << std::endl;
+    std::cout << "#############################################################################################################" << std::endl;
+    std::cout << "#############################################################################################################" << std::endl;
+    std::cout << std::endl;
+
+    return rootChain;
+
+}
 
 
 
@@ -23,33 +66,81 @@ void perf(int nThreads) {
 
     std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
 
+
+
     auto reconstructedParticle = [] (ROOT::VecOps::RVec<int> index, ROOT::VecOps::RVec<edm4hep::ReconstructedParticleData> in) {
         return FCCAnalyses::ReconstructedParticle::get(index, in); };
 
     std::string testFile = "root://eospublic.cern.ch//eos/experiment/fcc/ee/generation/DelphesEvents/spring2021/IDEA/p8_ee_ZH_ecm240/events_101027117.root";
 
+    TChain* chainZH = retrieveData(sampleSize);
+
     ROOT::EnableImplicitMT(nThreads);
 
     std::string rootFiles = inputDir + std::string("*");
 
-    ROOT::RDataFrame df("events", rootFiles);
+    ROOT::RDataFrame df(*chainZH);
     auto df2 = df
     .Alias("Muon0", "Muon#0.index")
     .Define("muons", FCCAnalyses::ReconstructedParticle::get, {"Muon0", "ReconstructedParticles"})
-    .Define("selected_muons", FCCAnalyses::ReconstructedParticle::sel_pt(10.), {"muons"})
-    .Define("selected_muons_pt", FCCAnalyses::ReconstructedParticle::get_pt, {"selected_muons"})
+    .Define("selected_muons", FCCAnalyses::ReconstructedParticle::sel_pt(10.), {"muons"});
+//    .Define("selected_muons_pt", FCCAnalyses::ReconstructedParticle::get_pt, {"selected_muons"}) +++
+//
+//    .Define("selected_muons_y", FCCAnalyses::ReconstructedParticle::get_y, {"selected_muons"}) +++
+//
+//
+//    .Define("selected_muons_e", FCCAnalyses::ReconstructedParticle::get_e, {"selected_muons"}) +++
+//    .Define("zed_leptonic", FCCAnalyses::ReconstructedParticle::resonanceBuilder(91.), {"selected_muons"}) +++
+//    .Define("zed_leptonic_m", FCCAnalyses::ReconstructedParticle::get_mass, {"zed_leptonic"}) +++
+//    .Define("zed_leptonic_pt", FCCAnalyses::ReconstructedParticle::get_pt, {"zed_leptonic"}) +++
+//    .Define("zed_leptonic_recoil", FCCAnalyses::ReconstructedParticle::recoilBuilder(240.), {"zed_leptonic"}) +++
+//    .Define("zed_leptonic_recoil_m", FCCAnalyses::ReconstructedParticle::get_mass, {"zed_leptonic_recoil"}) +++
+//    .Define("zed_leptonic_charge", FCCAnalyses::ReconstructedParticle::get_charge, {"zed_leptonic"}) +++
+//    .Filter("zed_leptonic_recoil_m.size() > 0", "zed_leptonic_recoil_m") +++
+//    .Snapshot("events", std::string(outputDir) + std::string(outputFile), { +++
+//    "muons",
+//    "selected_muons",
+//    "selected_muons_pt",
+//    "selected_muons_y",
+//    "selected_muons_e",
+//    "zed_leptonic",
+//    "zed_leptonic_m",
+//    "zed_leptonic_pt",
+//    "zed_leptonic_recoil",
+//    "zed_leptonic_recoil_m",
+//    "zed_leptonic_charge"});
 
-    .Define("selected_muons_y", FCCAnalyses::ReconstructedParticle::get_y, {"selected_muons"})
+    auto df3 = df2
+    .Define("selected_muons_pt", FCCAnalyses::ReconstructedParticle::get_pt, {"selected_muons"});
 
+    auto df4 = df3
+    .Define("selected_muons_y", FCCAnalyses::ReconstructedParticle::get_y, {"selected_muons"});
 
-    .Define("selected_muons_e", FCCAnalyses::ReconstructedParticle::get_e, {"selected_muons"})
-    .Define("zed_leptonic", FCCAnalyses::ReconstructedParticle::resonanceBuilder(91.), {"selected_muons"})
-    .Define("zed_leptonic_m", FCCAnalyses::ReconstructedParticle::get_mass, {"zed_leptonic"})
-    .Define("zed_leptonic_pt", FCCAnalyses::ReconstructedParticle::get_pt, {"zed_leptonic"})
-    .Define("zed_leptonic_recoil", FCCAnalyses::ReconstructedParticle::recoilBuilder(240.), {"zed_leptonic"})
-    .Define("zed_leptonic_recoil_m", FCCAnalyses::ReconstructedParticle::get_mass, {"zed_leptonic_recoil"})
-    .Define("zed_leptonic_charge", FCCAnalyses::ReconstructedParticle::get_charge, {"zed_leptonic"})
-    .Filter("zed_leptonic_recoil_m.size() > 0", "zed_leptonic_recoil_m")
+    auto df5 = df4
+    .Define("selected_muons_e", FCCAnalyses::ReconstructedParticle::get_e, {"selected_muons"});
+
+    auto df6 = df5
+    .Define("zed_leptonic", FCCAnalyses::ReconstructedParticle::resonanceBuilder(91.), {"selected_muons"});
+
+    auto df7 = df6
+    .Define("zed_leptonic_m", FCCAnalyses::ReconstructedParticle::get_mass, {"zed_leptonic"});
+
+    auto df8 = df7
+    .Define("zed_leptonic_pt", FCCAnalyses::ReconstructedParticle::get_pt, {"zed_leptonic"});
+
+    auto df9 = df8
+    .Define("zed_leptonic_recoil", FCCAnalyses::ReconstructedParticle::recoilBuilder(240.), {"zed_leptonic"});
+
+    auto df10 = df9
+    .Define("zed_leptonic_recoil_m", FCCAnalyses::ReconstructedParticle::get_mass, {"zed_leptonic_recoil"});
+
+    auto df11 = df10
+    .Define("zed_leptonic_charge", FCCAnalyses::ReconstructedParticle::get_charge, {"zed_leptonic"});
+
+    auto df12 = df11
+    .Filter("zed_leptonic_recoil_m.size() > 0", "zed_leptonic_recoil_m");
+
+    auto df13 = df12
     .Snapshot("events", std::string(outputDir) + std::string(outputFile), {
     "muons",
     "selected_muons",
@@ -63,12 +154,10 @@ void perf(int nThreads) {
     "zed_leptonic_recoil_m",
     "zed_leptonic_charge"});
 
-    auto df3 = df
-    .Alias("Muon0", "Muon#0.index")
-    .Define("muons", FCCAnalyses::ReconstructedParticle::get, {"Muon0", "ReconstructedParticles"})
-    .Define("selected_muons", FCCAnalyses::ReconstructedParticle::sel_pt(10.), {"muons"})
-    .Define("selected_muons_p", FCCAnalyses::ReconstructedParticle::get_e, {"selected_muons"});
-//    .Histo1D("selected_muons_pt");
+
+
+
+
 
 //    TCanvas* c = new TCanvas();
 //    df2->Print();
@@ -84,11 +173,14 @@ void perf(int nThreads) {
 
 int main(int args, char* argv[]) {
 
+    std::cout << "Start" << std::endl;
+
     int nCPUS = atoi(argv[1]);
 
     std::cout << "nThreads:" << nCPUS << std::endl;
 
     perf(nCPUS);
 
+    std::cout << "End" << std::endl;
     return 0;
 }
